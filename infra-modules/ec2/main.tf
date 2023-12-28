1b2cd29c89d68595fb81b7fb56da8d82a57c55e6
Jagat Pradhan <Jagat.Pradhan@blabla.com>
2023-12-28 08:30:39 +0200

    ec2 module update

--- infra-modules/ec2/main.tf
+++ infra-modules/ec2/main.tf
@@ -2,8 +2,13 @@ resource "aws_instance" "web" {
   ami = data.aws_ami.latest_linux.id
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.application_sg.id ]
+  tags = {
+    Name = "myEc2"
+  }
 }
 
 resource "aws_security_group" "application_sg" {
   name = "application_sg"
+  tags = {
+    Name = "mySG"
 }
