e31ae43f36cfeb5c5d3e2134e3c01cc85169dfa9
Jagat Pradhan <Jagat.Pradhan@blabla.com>
2023-12-28 07:27:22 +0200

    adding ec2 module

--- infra-modules/ec2/main.tf
+++ infra-modules/ec2/main.tf
@@ -1,6 +1,4 @@
 resource "aws_instance" "web" {
   ami = data.aws_ami.latest_linux.id
-  vpc_security_group_ids = var.security_group
-  security_groups = var.subnet_id
   instance_type = var.instance_type
 }
